34b9b1a70c2a3e7e1ffa0d75a42d855b5a38646a
vlajs16 <vladimir.belca@gmail.com>
2018-03-19 19:02:11 +0100

    Dodate su equals, toString i HashCode metode u klase Valuta i Kurs

--- Menjacnica/src/menjacnica/valuta/Valuta.java
+++ Menjacnica/src/menjacnica/valuta/Valuta.java
@@ -10,6 +10,43 @@ public class Valuta {
 	private ArrayList<Kurs> kursevi;
 	
 	
+	@Override
+	public String toString() {
+		return nazivValute + " (" + skraceniNaziv +")" + "\n"
+				+ kursevi.get(kursevi.size()-1).toString();
+	}
+	
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((skraceniNaziv == null) ? 0 : skraceniNaziv.hashCode());
+		return result;
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (skraceniNaziv == null) {
+			if (other.skraceniNaziv != null)
+				return false;
+		} else if (!skraceniNaziv.equals(other.skraceniNaziv))
+			return false;
+		return true;
+	}
+
+
+
 	public String getNazivValute() {
 		return nazivValute;
 	}
--- Menjacnica/src/menjacnica/valuta/kurs/Kurs.java
+++ Menjacnica/src/menjacnica/valuta/kurs/Kurs.java
@@ -1,5 +1,6 @@
 package menjacnica.valuta.kurs;
 
+import java.text.SimpleDateFormat;
 import java.util.GregorianCalendar;
 
 public class Kurs {
@@ -9,6 +10,49 @@ public class Kurs {
 	private double srednjiKurs;
 	
 	
+	@Override
+	public String toString() {
+		SimpleDateFormat sf = new SimpleDateFormat("dd.MM.yyy");
+		return sf.format(datum.getTime()) 
+				+ "\n\tProdajni kurs: " + prodajniKurs
+				+ "\n\tKupovni kurs: " + kupovniKurs
+				+ "\n\tSrednji kurs: " + srednjiKurs;
+	}
+	
+	
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		return result;
+	}
+
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Kurs other = (Kurs) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		return true;
+	}
+
+
+
+
 	public GregorianCalendar getDatum() {
 		return datum;
 	}
