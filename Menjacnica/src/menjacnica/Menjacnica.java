f7edcefb814dc85b91b7f1162b7c79d415d528b3
vlajs16 <vladimir.belca@gmail.com>
2018-03-19 20:35:30 +0100

    Implementirane su metode u Menjacnici, dodat je konstruktor u Kurs

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -14,19 +14,56 @@ public class Menjacnica implements MenjacnicaInterfejs {
 	@Override
 	public void dodavanjeKursa(GregorianCalendar datum, Valuta valuta, double prodajniKurs, double kupovniKurs,
 			double srednjiKurs) {
-		// TODO Auto-generated method stub
 		
+		Kurs novi = new Kurs(datum, prodajniKurs, kupovniKurs, srednjiKurs);
+		
+		if(valute.contains(valuta)) {
+			for(int i =0; i<valute.size(); i++) {
+				if(!valute.get(i).equals(valuta)) continue;
+				
+				valute.get(i).getKursevi().add(novi);
+				return;
+			}
+		}
+		
+		valuta.getKursevi().add(novi);
+		valute.add(valuta);
 	}
 
 	@Override
 	public void brisanjeKursa(GregorianCalendar datum, Valuta valuta) {
-		// TODO Auto-generated method stub
+		if(valuta.equals(null)) return;
+		if(!valute.contains(valuta)) return;
+		if(datum.equals(null)) return;
 		
+		for(int i = 0; i<valute.size(); i++) {
+			if(!valuta.equals(valute.get(i))) continue;
+			for(int j = 0; j<valute.get(i).getKursevi().size(); j++) {
+				if(!datum.equals(valute.get(i).getKursevi().get(j).getDatum())) continue;
+				
+				valute.get(i).getKursevi().remove(j);
+				return;
+			}
+		}
 	}
 
 	@Override
 	public Kurs pronalazenjeKursa(GregorianCalendar datum, Valuta valuta) {
-		// TODO Auto-generated method stub
+		if(valuta.equals(null)) 
+			throw new RuntimeException("Valuta je null");
+		if(!valute.contains(valuta)) 
+			throw new RuntimeException("Lista ne sadrzi ovu valutu");
+		if(datum.equals(null))
+			throw new RuntimeException("Datum je null");
+		
+		for(int i = 0; i<valute.size(); i++) {
+			if(!valuta.equals(valute.get(i))) continue;
+			for(int j = 0; j<valute.get(i).getKursevi().size(); j++) {
+				if(!datum.equals(valute.get(i).getKursevi().get(j).getDatum())) continue;
+				return valute.get(i).getKursevi().get(j);
+			}
+		}
+		
 		return null;
 	}
 
